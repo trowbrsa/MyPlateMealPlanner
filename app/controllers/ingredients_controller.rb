d498cd2e6eef6c5c31c7d3108bb27f8e92c378b4
emgord <emgord@gmail.com>
2015-11-23 22:10:51 -0800

    added new and create for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,23 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    Ingredient.create(ingredient_params[:ingredient])
+    redirect_to meal_path(params[:meal_id])
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def ingredient_params
+    params.permit(ingredient:[:ingredient_name, :food_group, :shopping_list, :meal_id])
+
+  end
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :meal
+
+  FOODGROUPS = ["Grains", "Protein", "Vegetables", "Fruits", "Dairy"]
 end
